42990adbf509f56a523786f88c916d1850f5829e
王事成 <wangshicheng@bytedance.com>
2023-06-14 15:43:07 +0800

    v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,17 @@
  */
 
 export interface IWindowOpen {
-  duration?: number
-  apiPromise: () => Promise<string>
-  errorCallback?: () => void
-  getErrorMsg?: (err: unknown) => string
-
   isClose?: boolean
+  duration?: number
+  errorPageUrl?: string
+  loadingPageUrl?: string
+
   errorStyle?: string
   loadingStyle?: string
+
+  apiPromise: () => Promise<string>
+  errorCallback?: (err: unknown) => void
+  getErrorMsg?: (err: unknown) => string
 }
 
 export const getErr = (err: unknown) => {
@@ -26,30 +29,61 @@ export const getErr = (err: unknown) => {
   return '存在异常'
 }
 
-const errorMsgCoponent = (params: { msg: string; duration: number; errorStyle?: string }) => {
-  const { msg, duration = 3, errorStyle } = params || {}
-  return `
-    <div style="margin-top: 8px; color: #232323; ${errorStyle}">
-      <div
-        style="padding: 8px;
-        text-align: center;"
-      >
-        <div
-          style="display: inline-block;
-        padding: 9px 12px;
-        background: #ffffff;
-        border-radius: 8px;
-        box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
-        pointer-events: all;"
-        >
-          <div>
-            <span>${msg || '异常错误消息'}</span>
-            <span id="window-open-error-msg">, ${duration}s之后关闭</span>
+const showErrorMsg = (params: {
+  msg: string
+  duration: number
+  errorStyle?: string
+  win: Window | null
+  errorPageUrl?: string
+}) => {
+  let { msg, duration = 3, errorStyle, win, errorPageUrl } = params || {}
+
+  if (!win) return
+
+  if (errorPageUrl) {
+    win.location.href = `${errorPageUrl}?errMsg=${msg}`
+    return
+  }
+
+  const timer = setInterval(function() {
+    if (duration > 0) {
+      duration--
+      if (!win) return
+      win.document.body.innerHTML = `
+        <div style="margin-top: 8px; color: #232323; ${errorStyle}">
+          <div
+            style="padding: 8px;
+            text-align: center;"
+          >
+            <div
+              style="display: inline-block;
+              padding: 9px 12px;
+              background: #ffffff;
+              border-radius: 8px;
+              box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
+              pointer-events: all;"
+            >
+              <div>
+                <span>${msg || '存在异常'}</span>
+                <span id="window-open-error-msg">, ${duration}秒跳转页面</span>
+              </div>
+            </div>
           </div>
-        </div>
       </div>
-  </div>
-  `
+      `
+    } else {
+      clearInterval(timer)
+    }
+  }, 1000)
+}
+
+const delay = (gap: number) => {
+  var then, now
+  then = new Date().getTime()
+  now = then
+  while (now - then < gap) {
+    now = new Date().getTime()
+  }
 }
 
 export const windowOpen: (params: IWindowOpen) => WindowProxy | null = params => {
@@ -60,43 +94,24 @@ export const windowOpen: (params: IWindowOpen) => WindowProxy | null = params =>
 
     isClose = true,
     duration = 3,
+    loadingPageUrl,
+    errorPageUrl,
     loadingStyle,
     errorStyle
   } = params || {}
   let win: Window | null = null
   const originHref = location.href
 
-  win = window.open('about:blank') // 打开一个空Tab页
-  if (!win) return win
-
-  win.document.body.innerHTML = `
-    <div style="margin-top: 8px; color: #232323; ${loadingStyle}">
-      <div style="padding: 8px;
-      text-align: center;">
-        <div
-        style="display: inline-block;
-      padding: 9px 12px;
-      background: #ffffff;
-      border-radius: 8px;
-      box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
-      pointer-events: all;"
-      >
-        页面加载中, 请稍后...
-      </div>
-    </div>
-  `
-
   apiPromise()
     ?.then(resUrl => {
       if (!win) return
-
       win.location.href = resUrl
     })
     ?.catch(err => {
       const msg = getErrorMsg?.(err)
 
       if (!win) return
-      win.document.body.innerHTML = errorMsgCoponent({ msg, duration, errorStyle })
+      showErrorMsg({ msg, duration, errorStyle, win, errorPageUrl })
 
       setTimeout(() => {
         if (!win) return
@@ -108,8 +123,33 @@ export const windowOpen: (params: IWindowOpen) => WindowProxy | null = params =>
         }
       }, duration * 1000)
 
-      errorCallback?.()
+      errorCallback?.(err)
     })
+
+  delay(3000)
+  win = window.open('about:blank') // 打开一个空Tab页
+  if (!win) return win
+
+  if (loadingPageUrl) {
+    win.location.href = loadingPageUrl
+  } else {
+    win.document.body.innerHTML = `
+    <div style="margin-top: 8px; color: #232323; ${loadingStyle}">
+      <div style="padding: 8px;
+      text-align: center;">
+        <div
+          style="display: inline-block;
+          padding: 9px 12px;
+          background: #ffffff;
+          border-radius: 8px;
+          box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
+          pointer-events: all;"
+        >
+          页面加载中, 请稍后...
+      </div>
+    </div>
+  `
+  }
 
   return win
 }
